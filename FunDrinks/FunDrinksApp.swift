c11c8870babe2f75949d64ec2c514981513448d7
Asel Zhumalieva <asel.zhumalieva27@gmail.com>
2024-11-28 15:36:29 -0600

    Initial Commit

---
+++ FunDrinks/FunDrinksApp.swift
@@ -0,0 +1,17 @@
+//
+//  FunDrinksApp.swift
+//  FunDrinks
+//
+//  Created by Asel Zhumalieva on 11/28/24.
+//
+
+import SwiftUI
+
+@main
+struct FunDrinksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
